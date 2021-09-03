09bc8f9adedb53e1f1b996472bc5b24ab7f96279
mikegomdi <mikegomez1493@gmail.com>
2021-09-03 14:53:53 -0500

    DAO Complete Example

--- src/main/java/datos/PersonaDAO.java
+++ src/main/java/datos/PersonaDAO.java
@@ -4,12 +4,12 @@ import static datos.Conexion.*;
 import domain.Persona;
 import java.sql.*;
 import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PersonaDAO {
     private static final String SQL_SELECT = "SELECT id_persona, nombre, apellido, email, telefono FROM test.persona";
-    
+    private static final String SQL_INSERT = "INSERT INTO persona(nombre, apellido, email, telefono) VALUES(?,?,?,?)";
+    private static final String SQL_UPDATE = "UPDATE persona SET nombre = ?, apellido = ?, email = ?, telefono = ? WHERE id_persona = ?";
+    private static final String SQL_DELETE = "DELETE FROM persona WHERE id_persona=?";
     // Método que regresa una lista de objetos de tipo Persona(clase Persona.java)
     public List<Persona> seleccionar(){
         //Inicializar parametros de conexión
@@ -54,4 +54,95 @@ public class PersonaDAO {
         }
         return personas;
     }
+    
+    public int insertar(Persona persona){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0; // Saber cuantos registros se han actualizado
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            //debido a los campos que se insertan se trabaja con tipo String
+            stmt.setString(1, persona.getNombre());//?
+            stmt.setString(2, persona.getApellido());//?
+            stmt.setString(3, persona.getEmail());//...
+            stmt.setString(4, persona.getTelefono());
+            
+            // Ejecutar sentencia
+            registros = stmt.executeUpdate(); // modifica el estado de la base de datos (insert, delete, update)
+            
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+    
+    public int actualizar(Persona persona){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0; // Saber cuantos registros se han actualizado
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            //debido a los campos que se insertan se trabaja con tipo String
+            stmt.setString(1, persona.getNombre());//?
+            stmt.setString(2, persona.getApellido());//?
+            stmt.setString(3, persona.getEmail());//...
+            stmt.setString(4, persona.getTelefono());
+            stmt.setInt(5, persona.getIdPersona()); // Where id_persona = ?
+                        
+            // Ejecutar sentencia
+            registros = stmt.executeUpdate(); // modifica el estado de la base de datos (insert, delete, update)
+            
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+    
+        public int eliminar(Persona persona){
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int registros = 0; // Saber cuantos registros se han actualizado
+        try {
+            conn = getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);           
+            stmt.setInt(1, persona.getIdPersona());
+                        
+            // Ejecutar sentencia
+            registros = stmt.executeUpdate(); // modifica el estado de la base de datos (insert, delete, update)
+            
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+        finally{
+            try {
+                close(stmt);
+                close(conn);
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return registros;
+    }
+    
 }
--- src/main/java/test/TestManejoDAOPersona.java
+++ src/main/java/test/TestManejoDAOPersona.java
@@ -7,16 +7,28 @@ import java.util.List;
 public class TestManejoDAOPersona {
 
     public static void main(String[] args) {
+        // Para acceder a los métodos del CRUD
         PersonaDAO personaDao = new PersonaDAO();
+        
+        // ´Para crear un modelo por medio de un objeto de tipo persona representado en la BD
+        // Insertar
+        /* Persona pesonaNueva = new Persona("Marvin", "Guzman", "mguzman@gmail.com", "4776058916");
+        personaDao.insertar(pesonaNueva);*/
+        
+        //Actualizar
+        /*Persona personaEditar = new Persona(1, "Luis Miguel", "Díaz", "luismi@gmail.com", "4778524696");
+        personaDao.actualizar(personaEditar);*/
+        
+        // Elminar
+        Persona personaEliminar = new Persona(4);
+        personaDao.eliminar(personaEliminar);
+        
+        // Traer datos               
         List<Persona> personas = personaDao.seleccionar();
-
         personas.forEach(persona -> {
             System.out.println(persona);
         });
-
-//        for (Persona persona:personas){
-//            System.out.println("persona"+persona);
-//        }
+        
     }
 
 }
